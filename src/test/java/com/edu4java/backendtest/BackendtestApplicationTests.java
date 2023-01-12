ff20ac4caaa7950c6726f164cc0962ea5234307b
Edu <edu@edu4java.com>
2023-01-12 19:27:09 +0100

    Spring Initializer

---
+++ src/test/java/com/edu4java/backendtest/BackendtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu4java.backendtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
